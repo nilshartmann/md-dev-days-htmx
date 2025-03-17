9fca794dc4fee90816a5aad1f181735c24d9847a
Nils Hartmann <nils@nilshartmann.net>
2025-03-17 13:54:06 +0100

    initial demo config

--- frontend_react/src/react-demo-config.tsx
+++ frontend_react/src/react-demo-config.tsx
@@ -14,7 +14,7 @@ export const enableTanstackQueryDevTools = false;
  * than searching for shorter terms
  * to show discarding of older requests
  */
-export const slowDown_search = 1200;
+export const slowDown_search = 0;
 
 /**
  */
@@ -33,4 +33,4 @@ export const slowDown_AddFeedback = 0;
 /** delay after submitting the newsletter form
  * note that there is a default delay of 250ms in the server
  * */
-export const slowDown_SubscribeNewsletter = 2400;
+export const slowDown_SubscribeNewsletter = 0;
